ff11840081e9346e99d465fc9fb9e1602acc82d3
gal-halevi <93094499+gal-halevi@users.noreply.github.com>
2023-04-23 15:36:51 +0300

    Update generate_workflow.py

--- .devops/generate_workflow.py
+++ .devops/generate_workflow.py
@@ -1,1 +1,11 @@
-print("Hello world")
+import json
+
+with open(".devops/manifest.json") as manifest_file:
+    manifest = json.load(manifest_file)
+
+with open(".github/workflows/checkout_manifest.yml", "w") as checkout:
+    components = []
+    for component in manifest.keys():
+        components.append(component)
+
+    checkout.write(str(components))
