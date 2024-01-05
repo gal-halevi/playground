a1969b5433554c32b6ef8d1bb1b67c16c851c224
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2024-01-06 01:16:51 +0200

    Fix

--- bad.py
+++ bad.py
@@ -1,3 +1,4 @@
-print "sdfsdfsdfdsfsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdff"
+print "sdfsdfsdfdsfsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdffsdfsdf"
 
-a = 5; b=6
+a = 5
+b = 6
